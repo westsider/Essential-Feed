099547275fa46284d947fbf913fed105ed1548ad
westsider <whansen1@mac.com>
2021-08-14 09:57:48 -0500

    Initial Commit

---
+++ Essential Feed/Essential_Feed.h
@@ -0,0 +1,18 @@
+//
+//  Essential_Feed.h
+//  Essential Feed
+//
+//  Created by Warren Hansen on 8/14/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Essential_Feed.
+FOUNDATION_EXPORT double Essential_FeedVersionNumber;
+
+//! Project version string for Essential_Feed.
+FOUNDATION_EXPORT const unsigned char Essential_FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Essential_Feed/PublicHeader.h>
+
+
---
+++ Essential FeedTests/Essential_FeedTests.swift
@@ -0,0 +1,33 @@
+//
+//  Essential_FeedTests.swift
+//  Essential FeedTests
+//
+//  Created by Warren Hansen on 8/14/21.
+//
+
+import XCTest
+@testable import Essential_Feed
+
+class Essential_FeedTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
